b841044ea07cc8f0deac333ab6b6cd72675cffc3
Dominic <dominicvana@gmail.com>
2021-03-16 12:01:09 +0000

    Confirm delete logic implemented

--- index.js
+++ index.js
@@ -37,13 +37,15 @@ class Contact {
       <div class="contact__field last-name">${this.lastName}</div>
       <div class="contact__field number">${this.number}</div>
       <div class="contact__field address">${this.address}</div>
-      <button class="contact__delete-btn">Delete</button>
+      <button class="contact__delete-btn material-icons">delete</button>
     `;
     this.className = 'contact';
   }
 
-  deleteContactHandler() {
-    event.target.parentElement.remove();
+  deleteContactHandler(event) {
+    const confirmationModal = new ConfirmDeleteModal(this.hookEl);
+    confirmationModal.render(event);
+    confirmationModal.animateIn();
   }
 
   render() {
@@ -55,7 +57,63 @@ class Contact {
       this.deleteContactHandler(event);
     });
     this.hookEl.append(contactEl);
-    // return contactEl;
+  }
+}
+
+class ConfirmDeleteModal {
+  constructor(hookEl) {
+    this.hookEl = hookEl;
+    this.innerHTML = `
+      <h3 class="contact__delete__confirm__heading">Are you sure?</h3>
+      <button class="contact__delete__confirm-btn">Delete</button>
+      <button class="contact__delete__cancel-btn">Cancel</button>
+    `;
+  }
+
+  animateIn() {
+    this.confirmDeleteModalEl.style.animation =
+      'slide-in 0.35s ease-out forwards';
+  }
+
+  animateOut() {
+    this.confirmDeleteModalEl.style.animation =
+      'slide-out 0.35s ease-in forwards';
+  }
+
+  confirmDeleteHandler(deletionEvent) {
+    this.confirmDeleteModalEl.style =
+      'animation-name: slide-out; animation-duration: 0.25s; animation-timing-function: ease';
+
+    setTimeout(() => {
+      App.deleteContact(deletionEvent);
+    }, 600);
+  }
+
+  cancelDeleteHandler(event) {
+    this.confirmDeleteModalEl.style =
+      'animation-name: slide-out; animation-duration: 0.25s; animation-timing-function: ease';
+  }
+
+  render(deletionEvent) {
+    this.confirmDeleteModalEl = document.createElement('div');
+    this.confirmDeleteModalEl.className = 'modal';
+    this.confirmDeleteModalEl.setAttribute('id', 'confirm-delete');
+    this.confirmDeleteModalEl.innerHTML = this.innerHTML;
+    const confirmDeleteBtn = this.confirmDeleteModalEl.querySelector(
+      '.contact__delete__confirm-btn'
+    );
+    const cancelDeleteBtn = this.confirmDeleteModalEl.querySelector(
+      '.contact__delete__cancel-btn'
+    );
+
+    confirmDeleteBtn.addEventListener('click', () => {
+      this.confirmDeleteHandler(deletionEvent);
+    });
+    cancelDeleteBtn.addEventListener('click', () => {
+      this.cancelDeleteHandler();
+    });
+
+    this.hookEl.append(this.confirmDeleteModalEl);
   }
 }
 
@@ -208,14 +266,12 @@ class SearchBar {
     });
 
     this.hookEl.append(searchBarEl);
-    // return searchBarEl;
   }
 }
 
-class ContactList {}
-
 class App {
   static createContactModal;
+  static deleteContactModal;
 
   static init() {
     const hook = document.getElementById('app');
@@ -223,12 +279,18 @@ class App {
     controlElement.render();
     this.createContactModal = new CreateContactModal(hook);
     this.createContactModal.render();
+    this.deleteContactModal = new ConfirmDeleteModal(hook);
+    this.deleteContactModal.render();
     const searchBar = new SearchBar(hook);
     searchBar.render();
   }
 
   static launchCreateContactModal() {
     this.createContactModal.modalAnimateIn();
+  }
+
+  static deleteContact(event) {
+    event.target.parentElement.remove();
   }
 }
 
