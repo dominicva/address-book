13b206561a0fe32d0153715f303a81085ad6ec38
Dominic <dominicvana@gmail.com>
2021-03-17 13:29:11 +0000

    CSS split into modules

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import './styles/main';
+import './styles/control';
+import './styles/contacts';
+import './styles/search';
+import './styles/animations';
+
 import App from './js/App';
 
 App.init();
